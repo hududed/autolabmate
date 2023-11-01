9069c6dbe97f256aacfb8a0a112242834ed2d416
hududed <hudwahab@gmail.com>
2023-11-01 07:53:06 -0600

    propose-metadata -> utils

--- pages/propose.py
+++ pages/propose.py
@@ -1,8 +1,12 @@
 import streamlit as st
-from utils import display_table, engine, inspect
-from st_pages import hide_pages
+from utils import (
+    engine,
+    inspect,
+    get_user_inputs,
+    validate_inputs,
+    display_dictionary,
+)
 import pandas as pd
-import numpy as np
 
 
 st.title("Propose Experiment")
@@ -13,107 +17,40 @@ def main():
     inspector = inspect(engine)
     table_names = inspector.get_table_names()
     if table_names:
-        table_name = st.selectbox('Select a table to display', table_names)
+        table_name = st.selectbox("Select a table to display", table_names)
 
         # Load the selected table
-        query = f'SELECT * FROM {table_name};'
+        query = f"SELECT * FROM {table_name};"
         table = pd.read_sql_query(query, engine)
-
-        # Get optimization type
-        optimization_type = st.selectbox('Select optimization type', ['single', 'multi'])
-
-        # Get output column names
-        if optimization_type == 'single':
-            output_column_names = [table.columns[-1]]
-        elif optimization_type == 'multi':
-            output_column_names = table.columns[-2:]
-
-        # Get number of parameters
-        num_parameters = len(table.columns) - len(output_column_names)
-
-        # Get number of random lines
-        num_random_lines = st.number_input('Enter the number of random lines', min_value=1, max_value=len(table))
-
-        # Get parameter info
-        parameter_info = table.dtypes[:-len(output_column_names)].to_dict()
-
-        # Get parameter ranges
-        parameter_ranges = {}
-        for column in table.columns[:-len(output_column_names)]:
-            if np.issubdtype(table[column].dtype, np.number):
-                min_value = st.number_input(f'Enter the min value for {column}', value=table[column].min())
-                max_value = st.number_input(f'Enter the max value for {column}', value=table[column].max())
-                parameter_ranges[column] = (min_value, max_value)
-            elif np.issubdtype(table[column].dtype, object):
-                categories = st.text_input(f'Enter the categories for {column}', value=', '.join(table[column].unique()))
-                parameter_ranges[column] = categories.split(', ')
-
+        (
+            optimization_type,
+            output_column_names,
+            num_parameters,
+            num_random_lines,
+            parameter_info,
+            parameter_ranges,
+        ) = get_user_inputs(table, table_name)
 
         # Add validate button
-        if st.button('Validate'):
-            validation_errors = []
-
-            # Validate numeric parameters
-            for column, range_values in parameter_ranges.items():
-                if np.issubdtype(table[column].dtype, np.number):
-                    min_value, max_value = range_values
-                    if not min_value <= table[column].max() <= max_value:
-                        validation_errors.append(f'Values for {column} are not within the specified range.')
-
-            # Validate string parameters
-            for column, categories in parameter_ranges.items():
-                if np.issubdtype(table[column].dtype, object):
-                    unique_values = table[column].unique()
-                    if not set(unique_values).issubset(set(categories)):
-                        validation_errors.append(f'Unique values for {column} are not within the specified categories.')
+        if st.button("Validate"):
+            validation_errors = validate_inputs(table, parameter_ranges)
 
             # Display validation errors or metadata
             if validation_errors:
                 for error in validation_errors:
                     st.write(error)
             else:
-                st.write('Validation passed.')
-                metadata = {
-                    'table_name': table_name,
-                    'optimization_type': optimization_type,
-                    'batch_type': 'batch',
-                    'output_column_names': output_column_names,
-                    'num_parameters': num_parameters,
-                    'num_random_lines': num_random_lines,
-                    'parameter_info': parameter_info,
-                    'parameter_ranges': parameter_ranges,
-                    'learner': 'regr.ranger',
-                    'acquisition_function': 'ei',
-                }
-                st.write(metadata)
+                st.write("Validation passed.")
+                display_dictionary(
+                    table_name,
+                    optimization_type,
+                    output_column_names,
+                    num_parameters,
+                    num_random_lines,
+                    parameter_info,
+                    parameter_ranges,
+                )
+
 
 if __name__ == "__main__":
     main()
-
-
-# AIM: create dictionary metadata for table created by inferring from data or user input
-
-
-
-# get_optimization_type -> user input: single (last column) or multi objective (2 last columns)
-
-# get_output_column_names -> if single objective, return last column name, multi objective return last 2 column names 
-
-# get_input_values -> infer: number of parameters (total column - output column(s)), user input: number of random lines
-
-# get_parameter_info -> infer: get parameter (column) name (total column - output column(s)), infer: parameter type
-
-
-# assume batch-optimization for now
-
-# get_parameter_ranges -> infer: data types from column values, if integer or float ->user input: min and max values for integer and float, if strings -> user input: categories for strings
-
-# validate if values are within range for integer and float, validate if unique string values are within categories
-
-# assume learner type is regression (regr.ranger for now )
-
-# assume expected improvement 
-
-# generate_dict_metadata -> dictionary with metadata accompanying data table / sample
-
-
--- utils.py
+++ utils.py
@@ -5,62 +5,71 @@ from sqlalchemy import create_engine, text, inspect
 import os
 import uuid
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import altair as alt
 
 
 load_dotenv()
 # Load Supabase credentials from .env file
-supabase_url = os.getenv('SUPABASE_URL')
-supabase_key = os.getenv('SUPABASE_KEY')
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
 
 # Initialize Supabase client
 supabase_client = supabase.create_client(supabase_url, supabase_key)
-PG_PASS = os.getenv('PG_PASS')
-DATABASE_URL = f'postgresql://postgres:{PG_PASS}@db.zugnayzgayyoveqcmtcd.supabase.co:5432/postgres'
+PG_PASS = os.getenv("PG_PASS")
+DATABASE_URL = (
+    f"postgresql://postgres:{PG_PASS}@db.zugnayzgayyoveqcmtcd.supabase.co:5432/postgres"
+)
 engine = create_engine(DATABASE_URL)
 
 
 # Define routes
 def create_account(email, password):
     # Create new account
-    response = supabase_client.auth.sign_up({
-        'email': email,
-        'password': password
-    })
+    response = supabase_client.auth.sign_up({"email": email, "password": password})
     return response
+
 
 def login(credentials):
     # Login to existing account
     response = supabase_client.auth.sign_in_with_password(credentials)
     return response
 
+
 def upload_to_bucket(bucket_name, file_name, file_content):
     # Generate new UUID for file name
-    new_file_name = str(uuid.uuid4()) + '-' + file_name
-    st.write(f'Uploading file "{file_name}" to bucket "{bucket_name}" as "{new_file_name}"')
+    new_file_name = str(uuid.uuid4()) + "-" + file_name
+    st.write(
+        f'Uploading file "{file_name}" to bucket "{bucket_name}" as "{new_file_name}"'
+    )
     # Upload file to bucket
     supabase_client.storage.from_(bucket_name).upload(new_file_name, file_content)
     st.write(f'"{new_file_name}" uploaded to bucket "{bucket_name}"')
 
+
 def create_table(table_name):
     # Create new table
     with engine.connect() as conn:
-        query = text(f'CREATE TABLE IF NOT EXISTS {table_name} (id UUID NOT NULL DEFAULT uuid_generate_v4(), PRIMARY KEY (id));')
+        query = text(
+            f"CREATE TABLE IF NOT EXISTS {table_name} (id UUID NOT NULL DEFAULT uuid_generate_v4(), PRIMARY KEY (id));"
+        )
         conn.execute(query)
         st.write(f'Table "{table_name}" created in database')
+
 
 def insert_data(table_name, data):
     # Insert data into table
     with engine.connect() as conn:
         df = pd.read_csv(data)
-        df.to_sql(table_name, conn, if_exists='replace', index=False)
+        df.to_sql(table_name, conn, if_exists="replace", index=False)
         st.write(f'{data.name} inserted into table "{table_name}"')
+
 
 def display_table(table_name):
     # Query the table
     with engine.connect() as conn:
-        query = f'SELECT * FROM {table_name}'
+        query = f"SELECT * FROM {table_name}"
         df = pd.read_sql(query, conn)
 
     # Highlight the maximum values in the last column
@@ -75,15 +84,110 @@ def display_table(table_name):
     # Plot output as a function of each parameter in the second column
     output = df.iloc[:, -1]
     for param in df.columns[:-1]:
-        chart_data = pd.DataFrame({
-            'x': df[param],
-            'y': output
-        })
-        chart = alt.Chart(chart_data).mark_circle().encode(
-            x=alt.X('x', title=param),
-            y=alt.Y('y', title=output.name)
+        chart_data = pd.DataFrame({"x": df[param], "y": output})
+        chart = (
+            alt.Chart(chart_data)
+            .mark_circle()
+            .encode(x=alt.X("x", title=param), y=alt.Y("y", title=output.name))
         )
         col2.altair_chart(chart)
-    sns.set_theme(context='talk')
-    pairplot_fig = sns.pairplot(df, hue="param3", diag_kind='kde')
+    sns.set_theme(context="talk")
+    pairplot_fig = sns.pairplot(df, hue="param3", diag_kind="kde")
     st.pyplot(pairplot_fig)
+
+
+def get_user_inputs(table, table_name):
+    # Get optimization type
+    optimization_type = st.selectbox("Select optimization type", ["single", "multi"])
+
+    # Get output column names
+    if optimization_type == "single":
+        output_column_names = [table.columns[-1]]
+    elif optimization_type == "multi":
+        output_column_names = table.columns[-2:]
+
+    # Get number of parameters
+    num_parameters = len(table.columns) - len(output_column_names)
+
+    # Get number of random lines
+    num_random_lines = st.number_input(
+        "Enter the number of random lines", min_value=1, max_value=len(table)
+    )
+
+    # Get parameter info
+    parameter_info = table.dtypes[: -len(output_column_names)].to_dict()
+
+    # Get parameter ranges
+    parameter_ranges = {}
+    for column in table.columns[: -len(output_column_names)]:
+        if np.issubdtype(table[column].dtype, np.number):
+            min_value = st.number_input(
+                f"Enter the min value for {column}", value=table[column].min()
+            )
+            max_value = st.number_input(
+                f"Enter the max value for {column}", value=table[column].max()
+            )
+            parameter_ranges[column] = (min_value, max_value)
+        elif np.issubdtype(table[column].dtype, object):
+            categories = st.text_input(
+                f"Enter the categories for {column}",
+                value=", ".join(table[column].unique()),
+            )
+            parameter_ranges[column] = categories.split(", ")
+
+    return (
+        optimization_type,
+        output_column_names,
+        num_parameters,
+        num_random_lines,
+        parameter_info,
+        parameter_ranges,
+    )
+
+
+def validate_inputs(table, parameter_ranges):
+    validation_errors = []
+
+    # Validate numeric parameters
+    for column, range_values in parameter_ranges.items():
+        if np.issubdtype(table[column].dtype, np.number):
+            min_value, max_value = range_values
+            if not min_value <= table[column].max() <= max_value:
+                validation_errors.append(
+                    f"Values for {column} are not within the specified range."
+                )
+
+    # Validate string parameters
+    for column, categories in parameter_ranges.items():
+        if np.issubdtype(table[column].dtype, object):
+            unique_values = table[column].unique()
+            if not set(unique_values).issubset(set(categories)):
+                validation_errors.append(
+                    f"Unique values for {column} are not within the specified categories."
+                )
+
+    return validation_errors
+
+
+def display_dictionary(
+    table_name,
+    optimization_type,
+    output_column_names,
+    num_parameters,
+    num_random_lines,
+    parameter_info,
+    parameter_ranges,
+):
+    metadata = {
+        "table_name": table_name,
+        "optimization_type": optimization_type,
+        "batch_type": "batch",
+        "output_column_names": output_column_names,
+        "num_parameters": num_parameters,
+        "num_random_lines": num_random_lines,
+        "parameter_info": parameter_info,
+        "parameter_ranges": parameter_ranges,
+        "learner": "regr.ranger",
+        "acquisition_function": "ei",
+    }
+    st.write(metadata)
