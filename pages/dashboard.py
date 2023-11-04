4fea3bfeb669acfdde791e8b9185d2fc423e3e47
hududed <hudwahab@gmail.com>
2023-11-04 00:25:15 -0600

    dashboard open session table

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 from utils import display_table, engine, inspect
 from st_pages import hide_pages
 
-# hide_pages(["home"])
 
 st.title("Dashboard")
 
@@ -11,10 +10,19 @@ def main():
     inspector = inspect(engine)
     table_names = inspector.get_table_names()
     if table_names:
-        table_name = st.selectbox('Select a table to display', table_names)
+        default_table = (
+            st.session_state.table_name
+            if "table_name" in st.session_state
+            else table_names[0]
+        )
+        table_name = st.selectbox(
+            "Select a table", table_names, index=table_names.index(default_table)
+        )
+        # table_name = st.selectbox('Select a table to display', table_names)
         display_table(table_name)
     else:
-        st.write('No tables found in the database.')
+        st.write("No tables found in the database.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
