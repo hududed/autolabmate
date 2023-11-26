3fa91e6475de45aa81ba118dce837dd865378e82
hududed <hudwahab@gmail.com>
2023-11-26 13:49:06 -0700

    debug

--- install.R
+++ install.R
@@ -1,10 +1,18 @@
 # List of packages to install
 packages <- c("mlr3mbo", "mlr3", "mlr3learners", "data.table", "tibble", "bbotk", "R.utils")
 
-# Function to install a package if it's not already installed
+
 install_if_not_installed <- function(package) {
-  if (!require(package, character.only = TRUE)) {
-    install.packages(package, repos = "http://cran.rstudio.com/", lib="/usr/lib/R/library")
+  lib_path <- "./lib/R/library"
+  
+  # Create the directory if it doesn't exist
+  if (!dir.exists(lib_path)) {
+    dir.create(lib_path, recursive = TRUE)
+  }
+  
+  # Install the package in the specified directory
+  if (!require(package, lib.loc = lib_path, character.only = TRUE)) {
+    install.packages(package, repos = "http://cran.rstudio.com/", lib = lib_path)
   }
 }
 
