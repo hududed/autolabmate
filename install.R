0bab598c7f863748924ce759ca21c2c913c0664b
hududed <hudwahab@gmail.com>
2023-11-26 09:23:23 -0700

    update lib path

--- install.R
+++ install.R
@@ -4,7 +4,7 @@ packages <- c("mlr3mbo", "mlr3", "mlr3learners", "data.table", "tibble", "bbotk"
 # Function to install a package if it's not already installed
 install_if_not_installed <- function(package) {
   if (!require(package, character.only = TRUE)) {
-    install.packages(package, repos = "http://cran.rstudio.com/")
+    install.packages(package, repos = "http://cran.rstudio.com/", lib="/usr/lib/R/site-library")
   }
 }
 
